1e5dcc97b3eec7f06b58e3ad07a63eef8dc99e7f
xue.zeng <xue.zeng@accenture.com>
2019-11-09 11:58:39 +0800

    add city list parse

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"regexp"
 
 	"golang.org/x/net/html/charset"
 	"golang.org/x/text/encoding"
@@ -30,7 +31,8 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%s\n", all)
+
+	printCityList(all)
 }
 
 // determineEncoding 猜测页面字符集编码
@@ -42,4 +44,13 @@ func determineEncoding(r io.Reader) encoding.Encoding {
 
 	e, _, _ := charset.DetermineEncoding(bytes, "")
 	return e
+}
+
+func printCityList(contents []byte) {
+	re := regexp.MustCompile(`<a href="(http://www.zhenai.com/zhenghun/[0-9a-z]+)"[^>]*>([^<]+)</a>`)
+	matches := re.FindAllSubmatch(contents, -1)
+	for _, m := range matches {
+		fmt.Printf("City: %s, URL: %s\n", m[2], m[1])
+	}
+	fmt.Printf("Matches found: %d\n", len(matches))
 }
